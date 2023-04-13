cdc7ce7d1c75735c3e1957cd5e0d0bcb766b19ad
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-13 10:59:03 +0100

    added checkout views and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'crispy_forms'
+    'crispy_forms',
     'home',
     'products',
     'bag',
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # requred by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import OrderLineItem, Order
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
